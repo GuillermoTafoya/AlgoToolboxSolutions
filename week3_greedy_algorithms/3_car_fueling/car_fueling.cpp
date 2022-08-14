eedb40bc84578c7a960c8fd3338ba47b0152548a
Guillermo Tafoya <A01633790@tec.mx>
2022-08-13 20:51:56 -0500

    Documented car fuel algorithm

--- week3_greedy_algorithms/3_car_fueling/car_fueling.cpp
+++ week3_greedy_algorithms/3_car_fueling/car_fueling.cpp
@@ -10,14 +10,19 @@ int compute_min_refills(int location, int dist, int tank, vector<int> & stops) {
     if (location + tank >= dist) {
         return 0;
     }
+    // If the past filter is not enough to reach the destination, check if the current filter is enough to reach the destination.
+    // If the stops is empty, given that the distance is not reachable, it is impossible to reach the destination.
+    // Also, if the next stop is not reachable, it is impossible to reach the destination.
     if (stops.size() == 0 || (stops[0] - location) > tank) {
         return INT_MAX;
     }
     int last_refill = location;
+    // Look for the furthest reachable stop.
     while (stops.size() > 0 && (stops[0] - last_refill) <= tank) {
         last_refill = stops[0];
         stops.erase(stops.begin());
     }
+    // Recursive call to compute the minimum number of refills.
     return 1 + compute_min_refills(last_refill, dist, tank, stops);
 }
 
