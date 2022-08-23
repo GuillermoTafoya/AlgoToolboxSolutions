4ad92db1465600647de4da5fe1325fca24b4b867
Guillermo Tafoya <A01633790@tec.mx>
2022-08-23 15:37:40 -0500

    3-7 solved

--- week3_greedy_algorithms/7_maximum_salary/largest_number.cpp
+++ week3_greedy_algorithms/7_maximum_salary/largest_number.cpp
@@ -7,8 +7,25 @@
 using std::vector;
 using std::string;
 
+/*
+As the last question of a successful interview, your boss gives you a few pieces of paper
+with numbers on it and asks you to compose a largest number from these numbers. The
+resulting number is going to be your salary, so you are very much interested in maximizing
+this number. How can you do this?
+
+
+Task. Compose the largest number out of a set of integers.
+
+Input Format. The first line of the input contains an integer 𝑛. The second line contains integers
+𝑎1, 𝑎2, . . . , 𝑎𝑛.
+
+Constraints. 1 ≤ 𝑛 ≤ 100; 1 ≤ 𝑎𝑖 ≤ 10^3 for all 1 ≤ 𝑖 ≤ 𝑛.
+*/
+
 string largest_number(vector<string> a) {
-  //write your code here
+  sort(a.begin(), a.end(), [](const string& a, const string& b) {
+    return a + b > b + a;
+  });
   std::stringstream ret;
   for (size_t i = 0; i < a.size(); i++) {
     ret << a[i];
